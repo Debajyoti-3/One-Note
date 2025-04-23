6ad4cf18d9efa7b5e37ae1ca29b623f21d4ef549
Debajyoti Mitra <debajyotimitra369@gmail.com>
2025-04-23 22:22:05 +0530

    fix:styling

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -20,12 +20,12 @@ function TodoForm() {
             <input
                 type="text"
                 placeholder="Write Todo..."
-                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-[#F6F8D5] py-1.5 text-black"
                 value={todo}
                 onChange={(e)=>setTodo(e.target.value)}
             />
 
-            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+            <button type="submit" className="rounded-r-lg px-3 py-1 bg-[#4F959D] text-white shrink-0 cursor-pointer">
                 Add
             </button>
         </form>
